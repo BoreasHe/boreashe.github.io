8ce6b1e5e2147ae24d3bc01b8b2ca738a51e9e67
Boreas He <boreashe.git@gmail.com>
2021-01-04 01:13:50 +0800

    Updates

---
+++ precache-manifest.ec10ad2bf718c5f634c0d527457f716b.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565c97c383a49f7952c6f15348ecb972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e4c7d23739b2299b996",
+    "url": "/static/css/main.a90b2983.chunk.css"
+  },
+  {
+    "revision": "7f7075391f087f0d9ba4",
+    "url": "/static/js/2.3140e4a4.chunk.js"
+  },
+  {
+    "revision": "77bd5056256cb01994b7af2667ce55df",
+    "url": "/static/js/2.3140e4a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4c7d23739b2299b996",
+    "url": "/static/js/main.dd5458f3.chunk.js"
+  },
+  {
+    "revision": "091dc72b492ee8c5bac9",
+    "url": "/static/js/runtime-main.c84adca6.js"
+  },
+  {
+    "revision": "ff21c7e359ee376608ac3c32ee769126",
+    "url": "/static/media/ArtofLife.ff21c7e3.jpg"
+  },
+  {
+    "revision": "034229170eceddca9e300e8d2a4c8208",
+    "url": "/static/media/FiraSans-Light.03422917.ttf"
+  },
+  {
+    "revision": "895f5b025a6cc4924b263f6beb06c777",
+    "url": "/static/media/FiraSans-Regular.895f5b02.ttf"
+  },
+  {
+    "revision": "6f73f323c1eb30db85ce30d092e45422",
+    "url": "/static/media/Hanabi.6f73f323.jpg"
+  },
+  {
+    "revision": "04af2a5aa0abf133036f8d70f0b9d58e",
+    "url": "/static/media/Kano.04af2a5a.otf"
+  },
+  {
+    "revision": "0d7134aa76d556ae5f077e3dfe303887",
+    "url": "/static/media/Onikiri.0d7134aa.jpg"
+  },
+  {
+    "revision": "781ff4fd83d1b1961fffc62faad34f61",
+    "url": "/static/media/PortfolioSite.781ff4fd.jpg"
+  },
+  {
+    "revision": "92790e0d139ae333a1837c6e62db5ee3",
+    "url": "/static/media/Repair-Grandson.92790e0d.jpg"
+  },
+  {
+    "revision": "d615509555a75b8fb3ad72735c95c6ef",
+    "url": "/static/media/Valhalla.d6155095.jpg"
+  },
+  {
+    "revision": "3191404a7803db8aeb32dab5480b543f",
+    "url": "/static/media/back-layer.3191404a.jpg"
+  },
+  {
+    "revision": "5ae92a8aaa55ac16d328116a240835f9",
+    "url": "/static/media/cursor.5ae92a8a.png"
+  },
+  {
+    "revision": "f7a7deb7d0a71c26f74aaef84e5a70a6",
+    "url": "/static/media/diamond.f7a7deb7.png"
+  },
+  {
+    "revision": "a561aa1c2b239408c270d6c3ed2e4ab9",
+    "url": "/static/media/dot-overlay.a561aa1c.png"
+  },
+  {
+    "revision": "2f032876af3d2b29a2a89064b57685f5",
+    "url": "/static/media/front-layer.2f032876.png"
+  },
+  {
+    "revision": "6668fb80d11730965cb16ab69f82893e",
+    "url": "/static/media/hanabi1.6668fb80.jpg"
+  },
+  {
+    "revision": "c60a449920ae0debf944cf6abbd48745",
+    "url": "/static/media/hanabi2.c60a4499.jpg"
+  },
+  {
+    "revision": "5870e30ac332fb4b5e5430fbdea62c35",
+    "url": "/static/media/hanabi3.5870e30a.jpg"
+  },
+  {
+    "revision": "6b845772c6e600c871b04c5c85fa154a",
+    "url": "/static/media/hanabi4.6b845772.jpg"
+  },
+  {
+    "revision": "3c1ee604cef636caf9e07b09d0f15fea",
+    "url": "/static/media/onikiri1.3c1ee604.jpg"
+  },
+  {
+    "revision": "0fffe53b4e9442641bb10e0a1088f064",
+    "url": "/static/media/onikiri2.0fffe53b.jpg"
+  },
+  {
+    "revision": "9cbadba3541499b747eb21fc3a7c00bd",
+    "url": "/static/media/onikiri3.9cbadba3.jpg"
+  },
+  {
+    "revision": "b0c024a411b522eaeca127a623dd5a43",
+    "url": "/static/media/repair-grandson1.b0c024a4.jpg"
+  },
+  {
+    "revision": "e1057ac54a296275a03ddc359e8074fc",
+    "url": "/static/media/repair-grandson2.e1057ac5.jpg"
+  },
+  {
+    "revision": "b1b6c48687fca8ff10f3fc13155eae5d",
+    "url": "/static/media/repair-grandson3.b1b6c486.jpg"
+  },
+  {
+    "revision": "e9dee06ae3a7d6b0acc5efb2cde573c1",
+    "url": "/static/media/valhalla1.e9dee06a.jpg"
+  },
+  {
+    "revision": "93885408bc7a86d75b9b15a0556b290f",
+    "url": "/static/media/valhalla2.93885408.jpg"
+  },
+  {
+    "revision": "d52509ecd96fe5babfe85fca594209d2",
+    "url": "/static/media/valhalla3.d52509ec.jpg"
+  },
+  {
+    "revision": "d6d5186740f0d5f1b79d2c39afc62695",
+    "url": "/static/media/valhalla4.d6d51867.jpg"
+  },
+  {
+    "revision": "ade63c2f934572267c5d97d2e734e13e",
+    "url": "/static/media/valhalla5.ade63c2f.jpg"
+  }
+]);
